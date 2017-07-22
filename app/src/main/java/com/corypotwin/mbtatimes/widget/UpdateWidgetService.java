9d078e189b83d1fb1b905bdc95a0a5d32dd56020
Cory Potwin <ctpotwin@Corys-MacBook-Pro.local>
2017-07-22 14:19:45 -0400

    Widget is fully functional

--- app/src/main/java/com/corypotwin/mbtatimes/widget/UpdateWidgetService.java
+++ app/src/main/java/com/corypotwin/mbtatimes/widget/UpdateWidgetService.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.corypotwin.mbtatimes.R;
+import com.corypotwin.mbtatimes.activities.CriteriaSearch;
+import com.corypotwin.mbtatimes.activities.MainActivity;
 
 import java.util.Random;
 
@@ -21,39 +23,46 @@ public class UpdateWidgetService extends Service {
 
     @Override
     public void onStart(Intent intent, int startId) {
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this
-                .getApplicationContext());
-
-        int[] allWidgetIds = intent
-                .getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
-
-        for (int widgetId : allWidgetIds) {
-            // create some random data
-            int number = (new Random().nextInt(100));
-
-            RemoteViews remoteViews = new RemoteViews(this
-                    .getApplicationContext().getPackageName(),
-                    R.layout.app_widget);
-            Log.w("WidgetExample", String.valueOf(number));
-            // Set the text
-            remoteViews.setTextViewText(R.id.textView,
-                    "Random: " + String.valueOf(number));
+//        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this
+//                .getApplicationContext());
+//
+//        int[] allWidgetIds = intent
+//                .getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
+//
+//        for (int widgetId : allWidgetIds) {
+//            // create some random data
+//            int number = (new Random().nextInt(100));
+//
+//            RemoteViews remoteViews = new RemoteViews(this
+//                    .getApplicationContext().getPackageName(),
+//                    R.layout.app_widget);
+//
+//            // Set the text
+//            remoteViews.setTextViewText(R.id.textView,
+//                    "Random: " + String.valueOf(number));
+//
+//            Intent inet = new Intent(this, MainActivity.class);
+//            inet.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            PendingIntent pIntentNetworkInfo = PendingIntent.getActivity(this, 0,
+//                    inet, 0);
+//            remoteViews.setOnClickPendingIntent(R.id.actionButton, pIntentNetworkInfo);
 
             // Register an onClickListener
-            Intent clickIntent = new Intent(this.getApplicationContext(),
-                    WidgetProvider.class);
+//            Intent clickIntent = new Intent(this, CriteriaSearch.class);
+//            Intent clickIntent = new Intent(this.getApplicationContext(),
+//                    WidgetProvider.class);
 
-            clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-            clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,
-                    allWidgetIds);
+//            clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+//            clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,
+//                    allWidgetIds);
 
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                    getApplicationContext(), 0, clickIntent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.textView, pendingIntent);
-            appWidgetManager.updateAppWidget(widgetId, remoteViews);
-        }
-        stopSelf();
+//            PendingIntent pendingIntent = PendingIntent.getBroadcast(
+//                    getApplicationContext(), 0, clickIntent,
+//                    PendingIntent.FLAG_UPDATE_CURRENT);
+//            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+//            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+//        }
+//        stopSelf();
 
         super.onStart(intent, startId);
     }
--- app/src/main/java/com/corypotwin/mbtatimes/widget/WidgetProvider.java
+++ app/src/main/java/com/corypotwin/mbtatimes/widget/WidgetProvider.java
@@ -1,38 +1,51 @@
 package com.corypotwin.mbtatimes.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.RemoteViews;
+
+import com.corypotwin.mbtatimes.R;
+import com.corypotwin.mbtatimes.activities.CriteriaSearch;
+import com.corypotwin.mbtatimes.activities.MainActivity;
+import com.corypotwin.mbtatimes.activities.MyRoutes;
+import com.corypotwin.mbtatimes.activities.NearbyStations;
+
+import static android.content.ContentValues.TAG;
+import static java.security.AccessController.getContext;
 
 /**
  * Created by ctpotwin on 6/25/17.
  */
 
 public class WidgetProvider extends AppWidgetProvider {
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
-            Log.w("paaah", "onUpdate method called");
-            // Get all ids
-            ComponentName thisWidget = new ComponentName(context,
-                    WidgetProvider.class);
-            int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                R.layout.app_widget);
 
-            // Build the intent to call the service
-            Intent intent = new Intent(context.getApplicationContext(),
-                    UpdateWidgetService.class);
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+        Intent myRoutes = new Intent(context, MyRoutes.class);
+        PendingIntent myRoutesPendingIntent = PendingIntent.getActivity(context, 0, myRoutes, 0);
+        remoteViews.setOnClickPendingIntent(R.id.my_routes_button, myRoutesPendingIntent);
 
-            // Update the widgets via the service
-            context.startService(intent);
-    }
+        Intent currentLocation = new Intent(context, NearbyStations.class);
+        PendingIntent currentLocationPendingIntent = PendingIntent.getActivity(context, 0, currentLocation, 0);
+        remoteViews.setOnClickPendingIntent(R.id.location_button, currentLocationPendingIntent);
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        super.onReceive(context, intent);
+        Intent searchStations = new Intent(context, CriteriaSearch.class);
+        PendingIntent searchStationsPendingIntent = PendingIntent.getActivity(context, 0, searchStations, 0);
+        remoteViews.setOnClickPendingIntent(R.id.search_button, searchStationsPendingIntent);
+
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+
     }
 
 }
